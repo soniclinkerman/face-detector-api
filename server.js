d9e153475c6ce2c3f0e5f71b0890902da1e8b389
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-05-01 17:52:28 -0400

    adding port

--- server.js
+++ server.js
@@ -48,8 +48,8 @@ app.put("/image", (req, res) => {image.handleImage(req,res, db)});
 
 app.post("/imageurl", (req, res) => {image.handleAPI(req,res)});
 
-app.listen(3001, ()=>{
-    console.log("I'm up on 3001")
+app.listen(process.env.port || 3000, () =>{
+    console.log(`App is running on ${process.env.port}`)
 });
 
 /*
