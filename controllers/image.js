77e1e990ca9f44f446f98323766853f08633adc3
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-05-01 17:35:59 -0400

    First commit

---
+++ controllers/image.js
@@ -0,0 +1,34 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '479f1e89ce1f400fb480b63805612332'
+   });
+
+const handleAPI = (req,res) => {
+    app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => 
+        {res.json(data)
+        }) 
+        .catch(err => res.status(400).json("Unable to work with api"))
+
+}
+
+
+
+const handleImage = (req,res, db) => {
+    const {id} = req.body;
+    db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0])
+    })
+    .catch(err => res.status(400).json("Unable to get entries"))
+}
+
+module.exports = {
+    handleImage: handleImage,
+    handleAPI: handleAPI
+}
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+//CORS
+const cors = require("cors");
+
+//KNEX FOR DATABASE CONNECTION
+const knex = require("knex");
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: "127.0.0.1",
+        user: "sonic",
+        password: "",
+        database: "face-detector"
+    }
+})
+
+const app = express();
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors())
+
+//CONTROLLERS
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image")
+
+
+
+//BCRYPT
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
+app.get("/", (req, res)=> {
+    res.send(database.users)
+})
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt, saltRounds)});
+
+app.post('/signin', (req,res) => {signin.handleSignIn(req,res,db,bcrypt)});
+
+app.get(`/profile/:id`, (req, res) => {profile.handleProfile(req,res,db)});
+
+app.put("/image", (req, res) => {image.handleImage(req,res, db)});
+
+app.post("/imageurl", (req, res) => {image.handleAPI(req,res)});
+
+app.listen(3001, ()=>{
+    console.log("I'm up on 3001")
+});
+
+/*
+/ --> This is working
+/signIn --> POST = Success/ Fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+*/
