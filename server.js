1daa64be6cbb89b66acb9632c9f65af17c9555d4
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-05-01 18:46:53 -0400

    remove node modules

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require("express");
+const express = require('express');
 //CORS
-const cors = require("cors");
+const cors = require('cors');
 
 //KNEX FOR DATABASE CONNECTION
-const knex = require("knex");
+const knex = require('knex');
 
 const db = knex({
     client: 'pg',
     connection: {
-        host: "127.0.0.1",
-        user: "sonic",
-        password: "",
-        database: "face-detector"
+        host: '127.0.0.1',
+        user: 'sonic',
+        password: '',
+        database: 'face-detector'
     }
 })
 
@@ -21,10 +21,10 @@ app.use(express.json());
 app.use(cors())
 
 //CONTROLLERS
-const register = require("./controllers/register");
-const signin = require("./controllers/signin");
-const profile = require("./controllers/profile");
-const image = require("./controllers/image")
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image')
 
 
 
@@ -34,28 +34,14 @@ const saltRounds = 10;
 const myPlaintextPassword = 's0/\/\P4$$w0rD';
 const someOtherPlaintextPassword = 'not_bacon';
 
-app.get("/", (req, res)=> {
-    res.send("Working")
-})
+app.get('/', (req, res)=> {res.send('It is working')})
 
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt, saltRounds)});
-
 app.post('/signin', (req,res) => {signin.handleSignIn(req,res,db,bcrypt)});
-
 app.get(`/profile/:id`, (req, res) => {profile.handleProfile(req,res,db)});
-
-app.put("/image", (req, res) => {image.handleImage(req,res, db)});
-
-app.post("/imageurl", (req, res) => {image.handleAPI(req,res)});
+app.put('/image', (req, res) => {image.handleImage(req,res, db)});
+app.post('/imageurl', (req, res) => {image.handleAPI(req,res)});
 
 app.listen(process.env.PORT || 3000, () =>{
-    console.log(`App is running on ${process.env.PORT}`)
-});
-
-/*
-/ --> This is working
-/signIn --> POST = Success/ Fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT --> user
-*/
+    console.log(`App is running on ${process.env.PORT}`)}
+)
