d687c47bccf9fa2a9d80751e58856d7c76dd66bb
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-05-01 20:34:33 -0400

    change

--- controllers/register.js
+++ controllers/register.js
@@ -1,34 +1,34 @@
-const handleRegister = (req, res, db, bcrypt) => {
-    const { email, name, password } = req.body;
+const handleRegister = (req,res,db, bcrypt, saltRounds) => {
+    const {name,email,password} = req.body;
     if (!email || !name || !password) {
-      return res.status(400).json('incorrect form submission');
+         return res.status(400).json("Incorrect form submission");
     }
-    const hash = bcrypt.hashSync(password);
-      db.transaction(trx => {
-        trx.insert({
-          hash: hash,
-          email: email
+    const hash = bcrypt.hashSync(password, saltRounds);
+    db.transaction(trx => {
+        return trx.insert({
+            hash: hash,
+            email: email
         })
         .into('login')
         .returning('email')
         .then(loginEmail => {
-          return trx('users')
+            return trx('users')
             .returning('*')
             .insert({
-              email: loginEmail[0],
-              name: name,
-              joined: new Date()
+                email: loginEmail[0],
+                name: name,
+                joined: new Date()
             })
             .then(user => {
-              res.json(user[0]);
+                res.json(user[0]);
             })
         })
         .then(trx.commit)
         .catch(trx.rollback)
-      })
-      .catch(err => res.status(400).json('unable to register'))
-  }
-  
-  module.exports = {
+    })
+    .catch(err => res.status(400).json("Unable to register"))
+}
+
+module.exports = {
     handleRegister: handleRegister
-  };
+}
